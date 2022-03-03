61e2e7b70fd4bac6c3f94a670dbefcc59bcb64fa
liujunjia <43411944+liujunjia1996@users.noreply.github.com>
2022-03-03 11:55:10 +0800

    Update configTemplate.js

--- configTemplate.js
+++ configTemplate.js
@@ -4,30 +4,36 @@ const getTemplate = (navbar,sidebar) => {
     const baseContent =
 `const { path } = require('@vuepress/utils')
 module.exports = {
-    lang: 'zh-CN',
-    title: "liujunjia's blog",
-    themeConfig: {
-        navbar: ${navbar},
-        sidebar: ${sidebar},
-    },
-    head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
-    bundler: '@vuepress/bundler-vite',
-    theme: path.resolve(__dirname, './theme'),
-    plugins: [
-      [
-        '@vuepress/plugin-search',
-        {
-          locales: {
-            '/': {
-              placeholder: 'Search',
-            },
-            '/zh/': {
-              placeholder: '搜索',
-            },
+  lang: 'zh-CN',
+  title: "liujunjia's blog",
+  themeConfig: {
+      navbar: ${navbar},
+      sidebar: ${sidebar},
+  },
+  head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+  bundler: '@vuepress/bundler-vite',
+  theme: path.resolve(__dirname, './theme'),
+  plugins: [
+    [
+      '@vuepress/plugin-search',
+      {
+        locales: {
+          '/': {
+            placeholder: 'Search',
+          },
+          '/zh/': {
+            placeholder: '搜索',
           },
         },
-      ],
+      },
     ],
+    [
+      '@vuepress/plugin-register-components',
+      {
+        componentsDir: path.resolve(__dirname, './components')
+      }
+    ]
+  ],
 }`
     return baseContent
 }
